6018e2d82dfc569e8db921bc516021bce4e9cfad
Hans Christian Reinl <info@drublic.de>
2013-12-10 13:32:34 +0100

    Initial development

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+### HEAD
+
+* Initial development
---
+++ config/htmlmin.js
@@ -0,0 +1,20 @@
+/**
+ * Minimize HTML code
+ */
+'use strict';
+
+var config = require('../../config');
+
+module.exports = {
+	dist: {
+		options: {
+			removeComments: true,
+			removeCommentsFromCDATA: true,
+			collapseBooleanAttributes: true,
+			removeAttributeQuotes: true,
+			removeRedundantAttributes: true
+		},
+
+		files: config.htmlmin
+	},
+};
